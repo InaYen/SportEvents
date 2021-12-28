36ba6be5a6c86d291457b69d87dd9060127cd47b
InaYen <minhyen12121999@gmail.com>
2021-12-28 20:39:47 +0200

    Database works!

--- SportEvents/Form1.cs
+++ SportEvents/Form1.cs
@@ -1,3 +1,5 @@
+using System.Data.SqlClient;
+
 namespace SportEvents
 {
     public partial class Form1 : Form
@@ -5,6 +7,13 @@ namespace SportEvents
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\minhy\source\repos\SportEvents\SportEvents\SportEventsDB.mdf;Integrated Security=True");
+            connection.Open();
+            connection.Close();
         }
     }
 }
